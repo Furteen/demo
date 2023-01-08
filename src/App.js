fcfb39ff86cf4b09251b02ea18c16aae99e9cc7e
Furteen <thinhphu2409@gmail.com>
2023-01-08 17:15:36 +0700

    react d

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+ 
+  return (
+  <div className=''>
+      <table>
+
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>A</td>
+            <td>12</td>
+          </tr>
+        </tbody>
+        
+      </table>
+  </div>
+  );
+}
+
+export default App;
